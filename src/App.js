f52b6b4b2add8e5319ba4e62e0ef8f6781a17653
unknown <albertkantwi@gmail.com>
2024-02-28 23:02:35 +0000

    Add components routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './index.css';
+import React from 'react';
+
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
+import {CTA, Brand, Navbar} from './components';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand/>
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+
     </div>
   );
 }
